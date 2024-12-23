55f0f471c01821b15b95ad9e0ab72cb195a53037
Grey Hill <infogreytech@gmail.com>
2024-12-23 11:24:21 -0800

    Fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import postcssNesting from "postcss-nesting";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "/lamport-clock-paper",
 	plugins: [preact()],
 	css: {
 		postcss: {
@@ -11,5 +12,5 @@ export default defineConfig({
 				postcssNesting,
 			]
 		}
-	}
+	},
 });
