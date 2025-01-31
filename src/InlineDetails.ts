aa0b3167034085ce1b8bfeca7fdb2a3bea27bdd7
Grey Hill <infogreytech@gmail.com>
2025-01-30 17:17:25 -0800

    Switch to more web components

---
+++ src/InlineDetails.ts
@@ -0,0 +1,59 @@
+import { css, html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+declare global {
+    interface HTMLElementTagNameMap {
+        "inline-details": InlineDetails;
+    }
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_templates_and_slots#a_more_involved_example
+@customElement("inline-details")
+export class InlineDetails extends LitElement {
+    @property({ type: Boolean, attribute: "open", reflect: true })
+    open: boolean = false;
+
+    _toggle() {
+        this.open = !this.open;
+    }
+
+    _close() {
+        this.open = false;
+    }
+
+    _open() {
+        this.open = true;
+    }
+
+    render() {
+        if (this.open) {
+            return html`<span class="open" @click=${this._close}>
+                <slot name="summary"></slot></span>
+            <div>
+                <slot name="content"></slot>
+            </div>`;
+        } else {
+            return html`<span class="closed" @click=${this._open}>
+                <slot name="summary"></slot></span>`;
+        }
+    }
+
+    static styles = css`
+    span {
+        text-decoration: underline dotted;
+        cursor: pointer;
+    }
+    span.open::before {
+        content: "\u{25BE}";
+    }
+    span.closed::before {
+        content: "\u{25B8}";
+    }
+
+    div {
+        padding: 0em 1em;
+        border: solid black 2px;
+        border-radius: 0.5em;
+    }
+    `;
+}
