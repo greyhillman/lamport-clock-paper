b8276ed7f09ae99290b05df152b071d80cef61ed
Grey Hill <infogreytech@gmail.com>
2025-02-03 16:06:27 -0800

    Add path highlight back in
    Only for pre-computed paths but it keeps things simpler (for now).

--- src/figure-highlights.ts
+++ src/figure-highlights.ts
@@ -35,3 +35,52 @@ for (const node of document.querySelectorAll("span[data-process]")) {
         }
     });
 }
+
+function addHighlightToPath(start: string, end: string, edges: [string, string][]) {
+    const vertices = new Set<string>();
+    edges.forEach(edge => {
+        vertices.add(edge[0]);
+        vertices.add(edge[1]);
+    });
+
+    const node = document.querySelector(`mrow[data-event-from="${start}"][data-event-to="${end}"]`);
+    const element = node as HTMLElement;
+
+    element.addEventListener("mouseenter", event => {
+        for (const vertex of vertices) {
+            for (const node of document.querySelectorAll(`[data-event="${vertex}"]`)) {
+                node.classList.add("highlight");
+            }
+        }
+
+        for (const edge of edges) {
+            for (const node of document.querySelectorAll(`[data-event-from="${edge[0]}"][data-event-to="${edge[1]}"]`)) {
+                node.classList.add("highlight");
+            }
+        }
+    });
+    element.addEventListener("mouseleave", event => {
+        for (const vertex of vertices) {
+            for (const node of document.querySelectorAll(`[data-event="${vertex}"]`)) {
+                node.classList.remove("highlight");
+            }
+        }
+
+        for (const edge of edges) {
+            for (const node of document.querySelectorAll(`[data-event-from="${edge[0]}"][data-event-to="${edge[1]}"]`)) {
+                node.classList.remove("highlight");
+            }
+        }
+    });
+}
+
+addHighlightToPath("p1", "r4", [
+    ["p1", "q2"],
+    ["q2", "q3"],
+    ["q3", "q4"],
+    ["q4", "r3"],
+    ["r3", "r4"],
+]);
+addHighlightToPath("p2", "p3", [
+    ["p2", "p3"],
+]);
